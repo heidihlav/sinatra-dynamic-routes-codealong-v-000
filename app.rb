baf0cab90aa48115d1a2d10426dfe62d1e105e3e
Heidi Hlavinka <hchlav@gmail.com>
2019-01-15 19:51:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,8 +20,8 @@ class App < Sinatra::Base
   end
 
   get "/multiply/:num1/:num2" do
-    params[:num1].to_i * params[:num2].to_i  
-    binding.pry
+    params[:num1].to_i * params[:num2].to_i
+    # binding.pry
   end
 
 end
